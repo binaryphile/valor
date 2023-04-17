cfa8e3298724557eaaa28083677a63749acc8e99
Ted Lilley <ted.lilley@digi.com>
2023-04-17 00:26:31 -0400

    newMember function

--- enum/Member.go
+++ enum/Member.go
@@ -1,17 +1,13 @@
 package enum
 
 type (
-	Member[T ~string] struct {
+	Member[T any] struct {
 		Enum[T]
 		v string
 	}
 )
 
 func (x Member[T]) String() string {
-	return x.v
-}
-
-func (x Member[T]) Name() string {
 	return x.v
 }
 
